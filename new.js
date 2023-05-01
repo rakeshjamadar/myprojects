c9f298111d045fef5a7c56e64e863314b36f733c
Rakesh Jamadar <rakesh.jamadar1995@gmail.com>
2023-05-01 17:19:02 +0530

    Added javascript code

--- new.js
+++ new.js
@@ -1,1 +1,9 @@
-console.log('hello Rakesh');
+document.querySelector('button').
+$('button').click(function() {
+    let getID = $(this).attr('id');
+    if ($('#hideImage'+getID).css('display') === 'none') {
+        $('#hideImage'+getID).show();
+    } else {
+        $('#hideImage'+getID).hide();
+    }
+})
